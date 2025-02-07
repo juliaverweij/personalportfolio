17f37563e1ea8b7612bce0181bc1397f7a424d20
juliaverweij <julia@verweij-smits.nl>
2025-02-07 22:16:59 +0100

    .

--- script.js
+++ script.js
@@ -3,39 +3,22 @@ function toggleNav(){
     document.getElementById("btn").classList.toggle('open');
   }
 
+let links = [
+    "https://juliaverweij.com/coffee.html",
+    "https://juliaverweij.com/doors.html",
+    "https://juliaverweij.com/paperjam.html",
+    "https://juliaverweij.com/julia.html",
+    "https://juliaverweij.com/index.html"
+];
+let randompage = Math.floor(Math.random() * links.length);
+let surprisepage = links[randompage];
 
-let text;
-  if (Math.random() < 0.5) {
-    text = "<a href='https://juliaverweij.com/coffee.html'>Random Project!</a>";
-  } else if (Math.random() < 1) {
-    text = "<a href='https://juliaverweij.com/doors.html'>Random Project!</a>";
-  } else if (Math.random() < 1.5) {
-    text = "<a href='https://juliaverweij.com/black-on-black.html'>Random Project!</a>";
-  } else if (Math.random() < 2) {
-    text = "<a href='https://juliaverweij.com/frustratie,opluchting.html'>Random Project!</a>";
-  } else if (Math.random() < 2.5) {
-    text = "<a href='https://juliaverweij.com/gallery.html'>Random Project!</a>";
-  } else if (Math.random() < 3) {
-    text = "<a href='https://juliaverweij.com/index.html'>Random Project!</a>";
-  } else if (Math.random() < 3.5) {
-    text = "<a href='https://juliaverweij.com/navigating-navigation.html'>Random Project!</a>";
-  } else if (Math.random() < 4) {
-    text = "<a href='https://juliaverweij.com/nerves-common-sense.html'>Random Project!</a>";
-  } else if (Math.random() < 4.5) {
-    text = "<a href='https://juliaverweij.com/one-step-at-a-time.html'>Random Project!</a>";
-  } else if (Math.random() < 5) {
-    text = "<a href='https://juliaverweij.com/portraits.html'>Random Project!</a>";
-  } else if (Math.random() < 5.5) {
-    text = "<a href='https://juliaverweij.com/unfamiliar.html'>Random Project!</a>";
-  } else {
-    text = "<a href='https://juliaverweij.com/paperjam.html'>Random Project!</a>";
-  }
-  document.getElementById("demo").innerHTML = text;
+function randomSurprise() {
+    window.location.replace(surprisepage);
+}
 
-  
 
 const surprise = document.getElementById("surpriseId");
-
 const toggleSurprise = () => surprise.classList.toggle("show");
 
 window.onclick = (event) => {
