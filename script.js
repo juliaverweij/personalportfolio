8a27bf3eae2773ce6c581b06c970c02e2754c24f
juliaverweij <julia@verweij-smits.nl>
2025-02-07 22:19:32 +0100

    .

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ function toggleNav(){
 let links = [
     "https://juliaverweij.com/coffee.html",
     "https://juliaverweij.com/doors.html",
-    "https://juliaverweij.com/paperjam.html",
-    "https://juliaverweij.com/julia.html",
-    "https://juliaverweij.com/index.html"
+    "https://juliaverweij.com/paperjam.html",,
+    "https://juliaverweij.com/black-on-black.html",
+    "https://juliaverweij.com/do-you-have-time.html",
+    "https://juliaverweij.com/frustratie,opluchting.html",
+    "https://juliaverweij.com/gallery.html",
+    "https://juliaverweij.com/navigating-navigation.html",
+    "https://juliaverweij.com/nerves-common-sense.html",
+    "https://juliaverweij.com/one-step-at-a-time.html",
+    "https://juliaverweij.com/portraits.html",
+    "https://juliaverweij.com/unfamiliar.html"
 ];
 let randompage = Math.floor(Math.random() * links.length);
 let surprisepage = links[randompage];
