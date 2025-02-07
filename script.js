31bb5b79b5b007f7ff7b73b36c6c7a233ca25b16
juliaverweij <julia@verweij-smits.nl>
2025-02-07 18:59:00 +0100

    .

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ window.onclick = (event) => {
 }
 
 surprise.addEventListener('click', (event) => event.stopPropagation());
+
+
+function openSite() {
+    var links = [
+                  "google.com",
+                  "youtube.com",
+                  "reddit.com",
+                  "apple.com"]
+    
+                openSite = function() {
+                  // get a random number between 0 and the number of links
+                  var randIdx = Math.random() * links.length;
+                  // round it, so it can be used as array index
+                  randIdx = parseInt(randIdx, 10);
+                  // construct the link to be opened
+                  var link = 'http://' + links[randIdx];
+                  };
+                  
+        return link;
+        
+        document.getElementById("link").innerHTML = openSite();
+    }
