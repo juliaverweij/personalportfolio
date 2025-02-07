8504eba0ecac6686c0528fb81c1b66b4af744b2b
juliaverweij <julia@verweij-smits.nl>
2025-02-07 18:02:09 +0100

    .

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+window.addEventListener("beforeunload", function () {
+    document.body.classList.add("animate-out");
+  });
+  
+  
 const surprise = document.getElementById("surpriseId");
 
 const toggleSurprise = () => surprise.classList.toggle("show");
