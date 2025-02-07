0a0b6669421df2d90142c1a895f56fe0a3d0983f
juliaverweij <julia@verweij-smits.nl>
2025-02-07 20:03:08 +0100

    .

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-const surprise = document.getElementById("surpriseId");
-
-const toggleSurprise = () => surprise.classList.toggle("show");
-
-window.onclick = (event) => {
-    if (!event.target.matches('.surprise')) {
-        if(surprise.classList.contains('show')){
-            surprise.classList.remove('show')
-        }
-    }
-}
-
-surprise.addEventListener('click', (event) => event.stopPropagation());
-
 function toggleNav(){
     document.getElementById("mySidepanel").classList.toggle('open');
     document.getElementById("btn").classList.toggle('open');
@@ -27,3 +13,18 @@ function toggleNav(){
     text = "<a href='https://juliaverweij.com/paperjam.html'>Random Project!</a>";
   }
   document.getElementById("demo").innerHTML = text;
+  
+
+  const surprise = document.getElementById("surpriseId");
+
+const toggleSurprise = () => surprise.classList.toggle("show");
+
+window.onclick = (event) => {
+    if (!event.target.matches('.surprise')) {
+        if(surprise.classList.contains('show')){
+            surprise.classList.remove('show')
+        }
+    }
+}
+
+surprise.addEventListener('click', (event) => event.stopPropagation());
