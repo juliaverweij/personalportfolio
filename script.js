81b5ad824f5febf32e486a6dedaaa4686c6afa0e
juliaverweij <julia@verweij-smits.nl>
2025-02-07 19:46:08 +0100

    .

--- script.js
+++ script.js
@@ -20,8 +20,10 @@ function toggleNav(){
 
   let text;
   if (Math.random() < 0.5) {
-    text = "<a href='https://juliaverweij.com/coffee.html'>Random Project</a>";
-  } else {
-    text = "<a href='https://juliaverweij.com/paperjam.html'>Random Project</a>";
+    text = "<a href='https://juliaverweij.com/coffee.html'>Random Project!</a>";
+  } else if (Math.random() < 1) {
+    text = "<a href='https://juliaverweij.com/doors.html'>Random Project!</a>";
+  }else {
+    text = "<a href='https://juliaverweij.com/paperjam.html'>Random Project!</a>";
   }
   document.getElementById("demo").innerHTML = text;
