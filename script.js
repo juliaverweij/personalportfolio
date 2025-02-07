40436c1e42b52ff6ec3f94d73c760b459fec2ccb
juliaverweij <julia@verweij-smits.nl>
2025-02-07 20:17:06 +0100

    .

--- script.js
+++ script.js
@@ -9,7 +9,25 @@ function toggleNav(){
     text = "<a href='https://juliaverweij.com/coffee.html'>Random Project!</a>";
   } else if (Math.random() < 1) {
     text = "<a href='https://juliaverweij.com/doors.html'>Random Project!</a>";
-  }else {
+  } else if (Math.random() < 1.5) {
+    text = "<a href='https://juliaverweij.com/black-on-black.html'>Random Project!</a>";
+  } else if (Math.random() < 2) {
+    text = "<a href='https://juliaverweij.com/frustratie,opluchting.html'>Random Project!</a>";
+  } else if (Math.random() < 2.5) {
+    text = "<a href='https://juliaverweij.com/gallery.html'>Random Project!</a>";
+  } else if (Math.random() < 3) {
+    text = "<a href='https://juliaverweij.com/index.html'>Random Project!</a>";
+  } else if (Math.random() < 3.5) {
+    text = "<a href='https://juliaverweij.com/navigating-navigation.html'>Random Project!</a>";
+  } else if (Math.random() < 4) {
+    text = "<a href='https://juliaverweij.com/nerves-common-sense.html'>Random Project!</a>";
+  } else if (Math.random() < 4.5) {
+    text = "<a href='https://juliaverweij.com/one-step-at-a-time.html'>Random Project!</a>";
+  } else if (Math.random() < 5) {
+    text = "<a href='https://juliaverweij.com/portraits.html'>Random Project!</a>";
+  } else if (Math.random() < 5.5) {
+    text = "<a href='https://juliaverweij.com/unfamiliar.html'>Random Project!</a>";
+  } else {
     text = "<a href='https://juliaverweij.com/paperjam.html'>Random Project!</a>";
   }
   document.getElementById("demo").innerHTML = text;
