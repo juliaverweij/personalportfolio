fae2a1d8bacc3e3a9800863f7683f76f6966aff7
juliaverweij <julia@verweij-smits.nl>
2025-02-07 19:30:28 +0100

    .

--- script.js
+++ script.js
@@ -16,3 +16,12 @@ function toggleNav(){
     document.getElementById("mySidepanel").classList.toggle('open');
     document.getElementById("btn").classList.toggle('open');
   }
+
+
+  let text;
+  if (Math.random() < 0.5) {
+    text = "<a href='https://juliaverweij.com/coffee.html'>Random Project</a>";
+  } else {
+    text = "<a href='https://juliaverweij.com/paperjam.html'>Random Project</a>";
+  }
+  document.getElementById("demo").innerHTML = text;
