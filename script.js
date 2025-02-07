ae13b59685c0640f88d1c6a0a2ff1be8d9348ba9
juliaverweij <julia@verweij-smits.nl>
2025-02-07 19:20:59 +0100

    .

--- script.js
+++ script.js
@@ -12,15 +12,6 @@ window.onclick = (event) => {
 
 surprise.addEventListener('click', (event) => event.stopPropagation());
 
-
-let text;
-if (Math.random() < 0.5) {
-  text = "<a href='https://juliaverweij.com/coffee.html'>Random Project</a>";
-} else {
-  text = "<a href='https://juliaverweij.com/paperjam.html'>Random Project</a>";
-}
-document.getElementById("demo").innerHTML = text;
-
 function toggleNav(){
     document.getElementById("mySidepanel").classList.toggle('open');
     document.getElementById("btn").classList.toggle('open');
